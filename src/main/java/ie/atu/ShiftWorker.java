016124a87d84a95012b6a297391bb048070a2d5b
Nathanperry057 <G00394355@atu.ie>
2022-12-12 09:44:14 +0000

    Added a new class that takes in the shift worker variables and uses similar getters and setters to the other class

---
+++ src/main/java/ie/atu/ShiftWorker.java
@@ -0,0 +1,62 @@
+package ie.atu;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ShiftWorker {
+
+    private int shift;
+    private int payroll;
+    private double payRate = 11.25;
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("You'll start your shift at: ");
+        Random r = new Random();
+        for (int shift = 0; shift < 1; shift++) {
+            System.out.println(r.nextInt(2) + 1);
+
+        }
+        System.out.println("Please enter your PPS: ");
+        int payroll = s.nextInt();
+        ShiftWorker sw = new ShiftWorker();
+        sw.setPayroll(payroll);
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+    public int getShift() {
+        return shift;
+    }
+
+    public void setShift(int shift) {
+        this.shift = shift;
+    }
+
+    public int getPayroll() {
+        return payroll;
+    }
+
+    public void setPayroll(int payroll) {
+        this.payroll = payroll;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(int payRate) {
+        this.payRate = payRate;
+    }
+}
