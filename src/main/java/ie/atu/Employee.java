30533bb3158d1c6c9e955b862688447f48be2265
Nathanperry057 <G00394355@atu.ie>
2022-12-12 09:10:10 +0000

    Added dependencies for testing with junit, created an employee class and added getters and setters

---
+++ src/main/java/ie/atu/Employee.java
@@ -0,0 +1,35 @@
+package ie.atu;
+
+public class Employee {
+
+    private String name;
+    private String id;
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+    public Employee() {
+        String name = "";
+        String id = "";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
