f2989cc4dd8c6e695c43e19d8f2e0eed73e466cd
Nathanperry057 <G00394355@atu.ie>
2022-12-12 09:23:36 +0000

    Added a main constructor and entered added Employee e1 to pass through the getter and setter values into the main constructor

--- src/main/java/ie/atu/Employee.java
+++ src/main/java/ie/atu/Employee.java
@@ -1,15 +1,34 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class Employee {
 
     private String name;
     private String id;
 
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the Employee name ");
+        String name = s.nextLine();
+        Employee e1 = new Employee();
+        e1.setName(name);
 
+        System.out.println("Enter the employee id ");
+        String id = s.nextLine();
+        e1.setId(id);
+
+        System.out.println("Employee 1");
+        System.out.println("Name: " + e1.getName());
+        System.out.println("ID: " + e1.getId());
     }
 
 
+    public void getReportDetails() {
+        System.out.println("Employee 1: " + getName());
+        getName();
+        getId();
+    }
 
 
     public Employee() {
